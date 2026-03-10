98cc3f34c7f0c8830eab20da44bec32d0ea6d967
Peter Vos <pjmvos@gmail.com>
2026-03-10 13:43:47 +0100

    simplify opening external links

--- utils/linktarget.lua
+++
@@ -1,9 +0,0 @@
---- linktarget.lua – add "target='_blank'" to external links so they open in a new tab
---- Peter Vos, 2025-09-11
-
-function Link(link)
-    if link.target:match '^https?%:' then
-      link.attributes.target = '_blank'
-      return link
-    end
-end
